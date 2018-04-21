9040bc691a96dd28326ed3c67580d36301099be0
NyuTartaros <niuyunfei123@live.cn>
2018-04-22 00:02:23 +0800

    fix

--- src/lst.cpp
+++ src/lst.cpp
@@ -11,6 +11,9 @@
 /*
  * LSTNode Implementation
  */
+/**
+ * Constructor
+ */
 LSTNode::LSTNode(){
 	leftchild = NULL;
 	rightchild = NULL;
@@ -18,6 +21,9 @@ LSTNode::LSTNode(){
 	s = NULL;
 }
 
+/**
+ * Constructor
+ */
 LSTNode::LSTNode(int j, double s){
 	leftchild = NULL;
 	rightchild = NULL;
@@ -25,36 +31,60 @@ LSTNode::LSTNode(int j, double s){
 	this->s = s;
 }
 
+/**
+ * Destructor
+ */
 LSTNode::~LSTNode(){
 	getLeftChild().deleteNode();
 	getRightChild().deleteNode();
 }
 
+/**
+ * 获得左子树
+ */
 LSTNode LSTNode::getLeftChild(){
 	return &leftchild;
 }
 
+/**
+ * 获得右子树
+ */
 LSTNode LSTNode::getRightChild(){
 	return &rightchild;
 }
 
+/**
+ * 设置左子节点
+ */
 void LSTNode::setLeftChild(int j, double s){
 	leftchild = new LSTNode(j, s);
 }
 
+/**
+ * 设置右子节点
+ */
 void LSTNode::setRightChild(int j, double s){
 	rightchild = new LSTNode(j, s);
 }
 
 //TODO Recursive delete
+/**
+ * 删除以结点为根的子树
+ */
 void LSTNode::deleteNode(){
 
 }
 
+/**
+ * 获得当前节点的最佳变量
+ */
 int LSTNode::getJ(){
 	return j;
 }
 
+/**
+ * 获得当前节点的最佳分割点
+ */
 double LSTNode::getS(){
 	return s;
 }
@@ -64,34 +94,58 @@ double LSTNode::getS(){
 /*
  * LST Implementation
  */
+/**
+ * Constructor
+ */
 LST::LST(){
-
+	head = NULL;
 }
 
+/**
+ * Destructor
+ */
 LST::~LST(){
+	head->deleteNode();
+}
+
+/**
+ * 获得左子节点的分割区间的输出
+ */
+double LST::getLeftOutput(int j, double s, double** x, double*y){
 
 }
 
-void LST::getLeftOutput(int j, double s, double** x, double*y){
+/**
+ * 获得右子节点的分割区间的输出
+ */
+double LST::getRightOutput(int j, double s, double** x, double*y){
 
 }
 
-void LST::getRightOutput(int j, double s, double** x, double*y){
-
-}
-
+/**
+ * 获得左子节点的分割区间的变量集合
+ */
 void LST::getLeftSet(int j, double s, double** x, double* y){
 
 }
 
+/**
+ * 获得右子节点的分割区间的变量集合
+ */
 void LST::getRightSet(int j, double s, double** x, double* y){
 
 }
 
+/**
+ * 训练函数
+ */
 void LST::fit(double** x, double* y){
 
 }
 
+/**
+ * 预测函数
+ */
 void LST::predict(double** x, double* y){
 
 }
--- src/lst.h
+++ src/lst.h
@@ -8,12 +8,17 @@
 #ifndef LST_H_
 #define LST_H_
 
+/**
+ * 最小二乘回归树，结点类
+ */
 class LSTNode{
 private:
 	LSTNode* leftchild;
 	LSTNode* rightchild;
 	int j;
+	// 最优变量
 	double s;
+	// 最优分割点
 public:
 	LSTNode();
 	LSTNode(int j, double s);
@@ -27,19 +32,41 @@ public:
 	double getS();
 };
 
+/**
+ * 最小二乘回归树
+ */
 class LST{
 private:
 
 	LSTNode* head;
-	void getLeftOutput(int j, double s, double** x, double*y);
-	void getRightOutput(int j, double s, double** x, double*y);
+	// 头结点的指针
+	/**
+	 * 获得左子集的输出
+	 */
+	double getLeftOutput(int j, double s, double** x, double*y);
+	/**
+	 * 获得右子集的输出
+	 */
+	double getRightOutput(int j, double s, double** x, double*y);
+	/**
+	 * 获得左子集的样本
+	 */
 	void getLeftSet(int j, double s, double** x, double* y);
+	/**
+	 * 获得右子集的样本
+	 */
 	void getRightSet(int j, double s, double** x, double* y);
 
 public:
 	LST();
 	~LST();
+	/**
+	 * 训练函数
+	 */
 	void fit(double** x, double* y);
+	/**
+	 * 预测函数
+	 */
 	void predict(double** x, double* y);
 };
 
